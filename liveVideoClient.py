4c4f5c62a80855e21fc87604d14f6be96ba933e9
CalvinTAVN <cn1182@princeton.edu>
2025-04-12 17:22:23 -0400

    improed code

--- liveVideoClient.py
+++ liveVideoClient.py
@@ -42,18 +42,17 @@ sock_file = client.makefile(mode='r')
 
 try:
     while True:
-        
         for line in sock_file:
             if not line:
                 print("server broke")
                 break
             parsed = json.loads(line)
-            print("parsed: ", parsed)
-        true_vec_unit = np.array(parsed)
-        print(true_vec_unit)
-        motion = input("Enter 'r' for rolling, 't' for spinning, 'c' for constant field,  or 's' to stop:")
-        if (motion == 's'):
-            break
+            true_vec_unit = np.array(parsed)
+            print(true_vec_unit)
+            motion = input("Enter 'r' for rolling, 't' for spinning, 'c' for constant field,  or 's' to stop:")
+            if (motion == 's'):
+                break
+        break
 finally:
     print("breaking everything")
     bus.shutdown()
